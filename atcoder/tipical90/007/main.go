8fc51f4bc51b1a37dcbe6b298eae901a1db6357f
KokiYamada <nino02000@icloud.com>
2023-04-26 02:12:27 +0000

    rewrite 007

--- atcoder/tipical90/007/main.go
+++ atcoder/tipical90/007/main.go
@@ -23,28 +23,32 @@ func main() {
 		a[i] = NextInt(sc)
 	}
 	q := NextInt(sc)
-	b := make([]int, q)
-	for i := range b {
-		b[i] = NextInt(sc)
-	}
 
 	sort.Slice(a, func(i, j int) bool { return a[i] < a[j] })
-	for j := range b {
-		i := sort.Search(n, func(i int) bool { return a[i] >= b[j] })
-		if i > len(a)-1 {
-			fmt.Println(dissatisfaction(a[i-1], b[j]))
+	for j := 0; j < q; j++ {
+		b := NextInt(sc)
+		// binary search
+		i := sort.Search(n, func(i int) bool { return a[i] >= b })
+
+		if i == n { // does not exist
+			fmt.Println(dissatisfaction(a[i-1], b))
 		} else if i == 0 {
-			fmt.Println(dissatisfaction(a[i], b[j]))
+			fmt.Println(dissatisfaction(a[i], b))
 		} else {
-			if dissatisfaction(a[i], b[j]) < dissatisfaction(a[i-1], b[j]) {
-				fmt.Println(dissatisfaction(a[i], b[j]))
-			} else {
-				fmt.Println(dissatisfaction(a[i-1], b[j]))
-			}
+			fmt.Println(min(dissatisfaction(a[i], b), dissatisfaction(a[i-1], b)))
 		}
 	}
 }
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// 不満度
 func dissatisfaction(target, current int) int {
 	val := math.Abs(float64(target - current))
 	return int(val)
