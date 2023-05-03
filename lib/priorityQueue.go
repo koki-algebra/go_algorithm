a3fcefc669f49d134d4f731096a500b81f00634a
KokiYamada <nino02000@icloud.com>
2023-05-03 08:37:44 +0000

    add priority queue

---
+++ lib/priorityQueue.go
@@ -0,0 +1,31 @@
+package lib
+
+type Item[T any] struct {
+	Value    T
+	Priority int
+}
+
+type PriorityQueue[T any] []Item[T]
+
+func (pq PriorityQueue[T]) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue[T]) Less(i, j int) bool {
+	return pq[i].Priority > pq[j].Priority
+}
+
+func (pq PriorityQueue[T]) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue[T]) Push(x any) {
+	*pq = append(*pq, x.(Item[T]))
+}
+
+func (pq *PriorityQueue[T]) Pop() any {
+	n := len(*pq)
+	res := (*pq)[n-1]
+	*pq = (*pq)[:n-1]
+	return res
+}
---
+++ lib/priorityQueue_test.go
@@ -0,0 +1,50 @@
+package lib
+
+import (
+	"container/heap"
+	"testing"
+)
+
+func TestPriorityQueue(t *testing.T) {
+	// Some items and their priorities.
+	items := map[string]int{
+		"banana": 3, "apple": 2, "pear": 4,
+	}
+
+	// Create a priority queue, put the items in it, and
+	// establish the priority queue (heap) invariants.
+	pq := make(PriorityQueue[string], len(items))
+	i := 0
+	for value, priority := range items {
+		pq[i] = Item[string]{
+			Value:    value,
+			Priority: priority,
+		}
+		i++
+	}
+	heap.Init(&pq)
+
+	// Insert a new item
+	item := Item[string]{
+		Value:    "orange",
+		Priority: 5,
+	}
+	heap.Push(&pq, item)
+
+	exps := []Item[string]{
+		{Value: "orange", Priority: 5},
+		{Value: "pear", Priority: 4},
+		{Value: "banana", Priority: 3},
+		{Value: "apple", Priority: 2},
+	}
+
+	for _, exp := range exps {
+		got := heap.Pop(&pq).(Item[string])
+		if got.Value != exp.Value {
+			t.Errorf("expected value is %s, but got %s", exp.Value, got.Value)
+		}
+		if got.Priority != exp.Priority {
+			t.Errorf("expected priority is %d, but got %d", exp.Priority, got.Priority)
+		}
+	}
+}
