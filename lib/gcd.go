df19a412cbe232566c190af6658731b7ed669a9a
Koki Yamada <nino02000@icloud.com>
2023-05-17 10:07:13 +0000

    add GCD

---
+++ lib/gcd.go
@@ -0,0 +1,8 @@
+package lib
+
+func Gcd(x, y int) int {
+	if y == 0 {
+		return x
+	}
+	return Gcd(y, x%y)
+}
---
+++ lib/gcd_test.go
@@ -0,0 +1,27 @@
+package lib
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGcd(t *testing.T) {
+	tests := []struct {
+		x, y int
+		exp  int
+	}{
+		{x: 1, y: 0, exp: 1},
+		{x: 15, y: 5, exp: 5},
+		{x: 11, y: 55, exp: 11},
+		{x: 1000000000000000000, y: 999999999999999999, exp: 1},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("(x,y)=(%d,%d)", test.x, test.y), func(t *testing.T) {
+			got := Gcd(test.x, test.y)
+			assert.Equal(t, test.exp, got)
+		})
+	}
+}
