b3a14eb29b7772043b7f585f241a58e200796283
KokiYamada <nino02000@icloud.com>
2023-05-20 10:12:10 +0000

    030

--- atcoder/tipical90/030/main.go
+++ atcoder/tipical90/030/main.go
@@ -6,4 +6,22 @@ func main() {
 	var n, k int
 	fmt.Scanf("%d %d", &n, &k)
 
+	numPrimeFactor := make([]int, n+1)
+	for i := 2; i <= n; i++ {
+		if numPrimeFactor[i] != 0 {
+			continue
+		}
+		for j := i; j <= n; j += i {
+			numPrimeFactor[j]++
+		}
+	}
+
+	ans := 0
+	for i := range numPrimeFactor {
+		if numPrimeFactor[i] >= k {
+			ans++
+		}
+	}
+
+	fmt.Println(ans)
 }
