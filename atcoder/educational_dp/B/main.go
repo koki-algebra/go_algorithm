9fbf38e9f62549f417d90311ae8176d44e9394c1
koki-algebra <yotsuba00040@gmail.com>
2023-11-26 03:27:31 +0900

    educational DP B

---
+++ atcoder/educational_dp/B/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	n, k := NextInt(sc), NextInt(sc)
+	h := make([]int, n)
+	for i := range h {
+		h[i] = NextInt(sc)
+	}
+
+	// dp[i]: 足場 i に辿り着くまでに支払うコストの総和の最小値 (i = 0, 1, ..., n-1)
+	dp := make([]int, n)
+	dp[1] = abc(h[1] - h[0])
+
+	for i := 2; i < n; i++ {
+		dp[i] = 1 << 60
+		for j := 1; j <= k && j <= i; j++ {
+			Chmin(&dp[i], dp[i-j]+abc(h[i]-h[i-j]))
+		}
+	}
+
+	fmt.Println(dp[n-1])
+}
+
+func abc(a int) int {
+	return int(math.Abs(float64(a)))
+}
+
+func NextInt(sc *bufio.Scanner) int {
+	if sc.Scan() {
+		n, err := strconv.Atoi(sc.Text())
+		if err != nil {
+			panic(err)
+		}
+		return n
+	}
+
+	if err := sc.Err(); err != nil {
+		panic(err)
+	}
+
+	return -1
+}
+
+func Chmin(a *int, b int) bool {
+	if *a > b {
+		*a = b
+		return true
+	}
+	return false
+}
