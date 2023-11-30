6c8468b9531eca06c931c3c15aa50dd63c7b8be6
koki-algebra <yotsuba00040@gmail.com>
2023-11-30 12:05:52 +0900

    fix ABC270 C

--- atcoder/abc/270/C/main.go
+++ atcoder/abc/270/C/main.go
@@ -75,29 +75,3 @@ func NextInt(sc *bufio.Scanner) int {
 
 	return -1
 }
-
-type Stack struct {
-	l *list.List
-}
-
-func NewStack() *Stack {
-	return &Stack{
-		l: list.New(),
-	}
-}
-
-func (s *Stack) Push(v any) {
-	s.l.PushFront(v)
-}
-
-func (s *Stack) Pop() any {
-	if s.l.Len() == 0 {
-		panic("stack is empty")
-	}
-	e := s.l.Front()
-	return s.l.Remove(e)
-}
-
-func (s *Stack) IsEmpty() bool {
-	return s.l.Len() == 0
-}
