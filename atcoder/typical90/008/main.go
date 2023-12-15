4c6d725144d75d536a7c69c89870da686937f3c3
koki-algebra <yotsuba00040@gmail.com>
2023-12-15 15:40:46 +0900

    typical90 008

--- atcoder/typical90/008/main.go
+++ atcoder/typical90/008/main.go
@@ -7,33 +7,22 @@ const (
 	T   = "atcoder"
 )
 
-// add a に b を足して MOD を取る
-func add(a *int, b int) {
-	*a += b
-	if *a >= MOD {
-		*a %= MOD
-	}
-}
-
 func main() {
 	var (
 		n int
 		s string
 	)
-	fmt.Scan(&n)
-	fmt.Scan(&s)
+	fmt.Scan(&n, &s)
 
-	// dp[i][j]: S の i 文字目までの部分文字列のうち, "atcoder" の j 文字目まで一致する方法の個数
 	dp := make([][]int, n+1)
 	for i := range dp {
 		dp[i] = make([]int, len(T)+1)
 	}
 	dp[0][0] = 1
 
-	// dp
 	for i := 0; i < n; i++ {
 		for j := 0; j <= len(T); j++ {
-			// S[i] を選ばない場合
+			// s[i] を選ばない場合
 			add(&dp[i+1][j], dp[i][j])
 
 			// s[i] を選ぶ場合
@@ -44,4 +33,12 @@ func main() {
 	}
 
 	fmt.Println(dp[n][len(T)])
+}
+
+// add a に b を足して MOD を取る
+func add(a *int, b int) {
+	*a += b
+	if *a >= MOD {
+		*a %= MOD
+	}
 }
