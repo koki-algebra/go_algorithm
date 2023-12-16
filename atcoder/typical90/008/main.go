b4969bbe8df2761b7aba3af765c64310c7279b96
koki-algebra <yotsuba00040@gmail.com>
2023-12-16 17:58:00 +0900

    typical90 008

--- atcoder/typical90/008/main.go
+++ atcoder/typical90/008/main.go
@@ -13,29 +13,27 @@ func main() {
 		s string
 	)
 	fmt.Scan(&n, &s)
+	m := len(T)
 
+	// dp[i][j]: 文字列 S の最初の i 文字から文字列かを抜き出してつなげる方法のうち, それが T の最初の j 文字まで一致するような方法の個数
 	dp := make([][]int, n+1)
-	for i := range dp {
-		dp[i] = make([]int, len(T)+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, m+1)
 	}
 	dp[0][0] = 1
 
 	for i := 0; i < n; i++ {
-		for j := 0; j <= len(T); j++ {
-			// s[i] を選ばない場合
+		for j := 0; j <= m; j++ {
 			add(&dp[i+1][j], dp[i][j])
-
-			// s[i] を選ぶ場合
-			if j < len(T) && s[i] == T[j] {
+			if j < m && s[i] == T[j] {
 				add(&dp[i+1][j+1], dp[i][j])
 			}
 		}
 	}
 
-	fmt.Println(dp[n][len(T)])
+	fmt.Println(dp[n][m])
 }
 
-// add a に b を足して MOD を取る
 func add(a *int, b int) {
 	*a += b
 	if *a >= MOD {
