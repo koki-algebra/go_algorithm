a6fcbfdbc5ba21e815ce525c286acda23652483c
koki-algebra <yotsuba00040@gmail.com>
2023-12-25 20:05:21 +0900

    typical90 026

--- atcoder/typical90/026/main.go
+++ atcoder/typical90/026/main.go
@@ -2,129 +2,103 @@ package main
 
 import (
 	"bufio"
-	"container/list"
 	"fmt"
 	"os"
 	"sort"
 	"strconv"
 )
 
-type Color int
-
-var (
-	sc     = bufio.NewScanner(os.Stdin)
-	whites []int
-	blacks []int
+type (
+	Color int
+	Graph [][]int
 )
 
 const (
-	unknown Color = iota
-	black
-	white
+	UNKNOWN Color = iota
+	BLACK
+	WHITE
 )
 
-type Graph [][]int
+var (
+	colors         []Color
+	whites, blacks []int
+	graph          Graph
+)
 
 func main() {
+	sc := bufio.NewScanner(os.Stdin)
 	sc.Split(bufio.ScanWords)
-	n := NextInt()
-	graph := make(Graph, n)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	n := NextInt(sc)
+	graph = make([][]int, n)
 	for i := 0; i < n-1; i++ {
-		a, b := NextInt()-1, NextInt()-1
+		a, b := NextInt(sc)-1, NextInt(sc)-1
 		graph[a] = append(graph[a], b)
 		graph[b] = append(graph[b], a)
 	}
 
-	dfs(graph, 0)
+	start := 0
+	colors = make([]Color, n)
+	colors[start] = WHITE
+	whites = append(whites, start)
+
+	dfs(start)
 
 	if len(whites) > len(blacks) {
-		sort.Slice(whites, func(i, j int) bool { return whites[i] < whites[j] })
+		sort.Ints(whites)
 		for i := 0; i < n/2; i++ {
-			fmt.Printf("%d ", whites[i]+1)
+			fmt.Fprintf(w, "%d ", whites[i]+1)
 		}
 	} else {
-		sort.Slice(blacks, func(i, j int) bool { return blacks[i] < blacks[j] })
+		sort.Ints(blacks)
 		for i := 0; i < n/2; i++ {
-			fmt.Printf("%d ", blacks[i]+1)
+			fmt.Fprintf(w, "%d ", blacks[i]+1)
 		}
 	}
-	fmt.Print("\n")
+	fmt.Fprintln(w)
 }
 
-func dfs(graph Graph, s int) bool {
-	colors := make([]Color, len(graph))
-	for i := range colors {
-		colors[i] = unknown
-	}
-	colors[s] = white
-	st := NewStack()
-	st.Push(s)
+func dfs(v int) {
+	for _, nv := range graph[v] {
+		if colors[nv] == UNKNOWN {
+			colors[nv] = colors[v].Invert()
 
-	whites = append(whites, s)
-
-	for !st.IsEmpty() {
-		v := st.Pop().(int)
-		for _, nv := range graph[v] {
-			if colors[nv] != unknown {
-				if colors[nv] == colors[v] {
-					return false
-				}
-			} else {
-				st.Push(nv)
-				colors[nv] = invertColor(colors[v])
-
-				if colors[nv] == white {
-					whites = append(whites, nv)
-				} else {
-					blacks = append(blacks, nv)
-				}
+			if colors[nv] == WHITE {
+				whites = append(whites, nv)
+			} else if colors[nv] == BLACK {
+				blacks = append(blacks, nv)
 			}
+
+			dfs(nv)
 		}
 	}
-
-	return true
 }
 
-func invertColor(color Color) Color {
-	if color == white {
-		return black
-	} else if color == black {
-		return white
+func (c Color) Invert() Color {
+	switch c {
+	case WHITE:
+		return BLACK
+	case BLACK:
+		return WHITE
+	default:
+		return UNKNOWN
 	}
-	return unknown
 }
 
-func NextInt() int {
-	sc.Scan()
-	n, err := strconv.Atoi(sc.Text())
-	if err != nil {
+func NextInt(sc *bufio.Scanner) int {
+	if sc.Scan() {
+		n, err := strconv.Atoi(sc.Text())
+		if err != nil {
+			panic(err)
+		}
+		return n
+	}
+
+	if err := sc.Err(); err != nil {
 		panic(err)
 	}
-	return n
-}
 
-type Stack struct {
-	l *list.List
-}
-
-func NewStack() *Stack {
-	return &Stack{
-		l: list.New(),
-	}
-}
-
-func (s *Stack) Push(v interface{}) {
-	s.l.PushFront(v)
-}
-
-func (s *Stack) Pop() interface{} {
-	if s.l.Len() == 0 {
-		panic("stack is empty")
-	}
-	e := s.l.Front()
-	return s.l.Remove(e)
-}
-
-func (s *Stack) IsEmpty() bool {
-	return s.l.Len() == 0
+	return -1
 }
