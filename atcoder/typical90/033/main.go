be886ac5758387e5527813e03c9b1ac37559e1dd
koki-algebra <yotsuba00040@gmail.com>
2023-12-27 18:30:10 +0900

    typical90 033

--- atcoder/typical90/033/main.go
+++ atcoder/typical90/033/main.go
@@ -4,31 +4,11 @@ import "fmt"
 
 func main() {
 	var h, w int
-	fmt.Scanf("%d %d", &h, &w)
-	area := make([][]bool, h)
-	for i := range area {
-		area[i] = make([]bool, w)
-	}
+	fmt.Scan(&h, &w)
 
-	var countH, countW int
-	if h == 1 {
-		fmt.Println(w)
-		return
-	}
-	if w == 1 {
-		fmt.Println(h)
-		return
-	}
-	if h%2 == 0 {
-		countH = h / 2
+	if h == 1 || w == 1 {
+		fmt.Println(h * w)
 	} else {
-		countH = h/2 + 1
+		fmt.Println(((h + 1) / 2) * ((w + 1) / 2))
 	}
-	if w%2 == 0 {
-		countW = w / 2
-	} else {
-		countW = w/2 + 1
-	}
-
-	fmt.Println(countH * countW)
 }
