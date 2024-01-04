e65bf5328332e310c340f89feec25ef47370be22
koki-algebra <yotsuba00040@gmail.com>
2024-01-04 17:20:08 +0900

    typical90 043

---
+++ atcoder/typical90/043/README.md
@@ -0,0 +1,6 @@
+# 043 - Maze Challenge with Lack of Sleep（★4）
+
+## テーマ: 拡張BFS
+
+## 解法
+https://github.com/E869120/kyopro_educational_90/blob/main/editorial/043.jpg
--- atcoder/typical90/043/main.go
+++ atcoder/typical90/043/main.go
@@ -5,95 +5,93 @@ import (
 	"container/list"
 	"fmt"
 	"os"
-	"strconv"
 )
 
-var (
-	sc = bufio.NewScanner(os.Stdin)
-	dx = []int{1, 0, -1, 0}
-	dy = []int{0, 1, 0, -1}
-)
-
-const (
-	INF = 1 << 60
-)
+const INF = 1 << 60
 
 type State struct {
-	x, y, dir int
+	r, c, dir int
 }
 
 func main() {
-	sc.Split(bufio.ScanWords)
-	h, w := NextInt(), NextInt()
-	sx, sy := NextInt()-1, NextInt()-1
-	gx, gy := NextInt()-1, NextInt()-1
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Buffer([]byte{}, 5e6)
+
+	var h, w, rs, cs, rt, ct int
+	fmt.Scan(&h, &w, &rs, &cs, &rt, &ct)
+	rs--
+	cs--
+	rt--
+	ct--
 	s := make([]string, h)
 	for i := range s {
-		s[i] = NextLine()
+		s[i] = NextLine(sc)
 	}
 
 	dist := make([][][]int, h)
-	for i := 0; i < h; i++ {
-		dist[i] = make([][]int, w)
-		for j := 0; j < w; j++ {
-			dist[i][j] = make([]int, 4)
-			dist[i][j][0] = INF
-			dist[i][j][1] = INF
-			dist[i][j][2] = INF
-			dist[i][j][3] = INF
+	for r := 0; r < h; r++ {
+		dist[r] = make([][]int, w)
+		for c := 0; c < w; c++ {
+			dist[r][c] = make([]int, 4)
+			for dir := 0; dir < 4; dir++ {
+				dist[r][c][dir] = INF
+			}
 		}
 	}
 
-	l := list.New()
-	for i := 0; i < 4; i++ {
-		dist[sx][sy][i] = 0
-		l.PushBack(State{x: sx, y: sy, dir: i})
+	dr := []int{0, 1, 0, -1}
+	dc := []int{1, 0, -1, 0}
+
+	deque := list.New()
+	for dir := 0; dir < 4; dir++ {
+		dist[rs][cs][dir] = 0
+		deque.PushBack(State{r: rs, c: cs, dir: dir})
 	}
 
-	for l.Len() != 0 {
-		u := l.Remove(l.Front()).(State)
-		for i := 0; i < 4; i++ {
-			tx := u.x + dx[i]
-			ty := u.y + dy[i]
-			cost := dist[u.x][u.y][u.dir]
-			if u.dir != i {
+	for deque.Len() != 0 {
+		v := deque.Remove(deque.Front()).(State)
+		for dir := 0; dir < 4; dir++ {
+			// Next row & column
+			nr := v.r + dr[dir]
+			nc := v.c + dc[dir]
+			cost := dist[v.r][v.c][v.dir]
+			if v.dir != dir {
 				cost++
 			}
 
-			if tx >= 0 && tx < h && ty >= 0 && ty < w && s[tx][ty] == '.' && dist[tx][ty][i] > cost {
-				dist[tx][ty][i] = cost
-				if u.dir != i {
-					l.PushBack(State{x: tx, y: ty, dir: i})
+			if nr >= 0 && nr < h && nc >= 0 && nc < w && s[nr][nc] == '.' && Chmin(&dist[nr][nc][dir], cost) {
+				state := State{r: nr, c: nc, dir: dir}
+				if v.dir != dir {
+					deque.PushBack(state)
 				} else {
-					l.PushFront(State{x: tx, y: ty, dir: i})
+					deque.PushFront(state)
 				}
 			}
 		}
 	}
 
 	ans := INF
-	for i := 0; i < 4; i++ {
-		chmin(&ans, dist[gx][gy][i])
+	for dir := 0; dir < 4; dir++ {
+		Chmin(&ans, dist[rt][ct][dir])
 	}
 	fmt.Println(ans)
 }
 
-func chmin(a *int, b int) {
-	if *a > b {
-		*a = b
+func NextLine(sc *bufio.Scanner) string {
+	if sc.Scan() {
+		return sc.Text()
 	}
-}
-
-func NextInt() int {
-	sc.Scan()
-	n, err := strconv.Atoi(sc.Text())
-	if err != nil {
+	if err := sc.Err(); err != nil {
 		panic(err)
 	}
-	return n
+
+	return ""
 }
 
-func NextLine() string {
-	sc.Scan()
-	return sc.Text()
+func Chmin(a *int, b int) bool {
+	if *a > b {
+		*a = b
+		return true
+	}
+	return false
 }
