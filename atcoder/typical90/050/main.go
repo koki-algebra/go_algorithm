282370f4afbf327455a23d30d71fdef0c299abc0
koki-algebra <yotsuba00040@gmail.com>
2024-01-07 16:20:10 +0900

    typical90 050

--- atcoder/typical90/050/main.go
+++ atcoder/typical90/050/main.go
@@ -2,26 +2,28 @@ package main
 
 import "fmt"
 
-const (
-	MOD = 1000000007
-)
+const MOD = 1000000007
 
 func main() {
 	var n, l int
-	fmt.Scanf("%d %d", &n, &l)
+	fmt.Scan(&n, &l)
 
 	dp := make([]int, n+1)
 	dp[0] = 1
+
 	for i := 1; i <= n; i++ {
+		add(&dp[i], dp[i-1])
 		if i >= l {
-			dp[i] = dp[i-1] + dp[i-l]
-		} else {
-			dp[i] = dp[i-1]
-		}
-		if dp[i] >= MOD {
-			dp[i] -= MOD
+			add(&dp[i], dp[i-l])
 		}
 	}
 
 	fmt.Println(dp[n])
+}
+
+func add(a *int, b int) {
+	*a += b
+	if *a > MOD {
+		*a -= MOD
+	}
 }
