757ef62f7148f7b3bc48b605bc6c16dde159116c
koki-algebra <yotsuba00040@gmail.com>
2024-02-02 00:16:04 +0900

    fix CI

--- atcoder/abc/267/B/main.go
+++ atcoder/abc/267/B/main.go
@@ -18,31 +18,44 @@ func main() {
 	}
 
 	// 5 が倒れている
-	if !isStanding[4] && (isStanding[1] || isStanding[3] || isStanding[7] || isStanding[6]) && (isStanding[2] || isStanding[5] || isStanding[8] || isStanding[9]) {
+	if !isStanding[4] &&
+		(isStanding[1] || isStanding[3] || isStanding[7] || isStanding[6]) &&
+		(isStanding[2] || isStanding[5] || isStanding[8] || isStanding[9]) {
 		fmt.Println("Yes")
 		return
 	}
 
 	// 2 と 8 が倒れている
-	if !isStanding[1] && !isStanding[7] && (isStanding[3] || isStanding[6]) && (isStanding[4] || isStanding[2] || isStanding[5] || isStanding[8] || isStanding[9]) {
+	if !isStanding[1] && !isStanding[7] &&
+		(isStanding[3] || isStanding[6]) &&
+		(isStanding[4] || isStanding[2] || isStanding[5] || isStanding[8] || isStanding[9]) {
 		fmt.Println("Yes")
 		return
 	}
 
 	// 4 が倒れている
-	if !isStanding[3] && isStanding[6] && (isStanding[1] || isStanding[2] || isStanding[4] || isStanding[5] || isStanding[7] || isStanding[8] || isStanding[9]) {
+	if !isStanding[3] && isStanding[6] &&
+		(isStanding[1] || isStanding[2] ||
+			isStanding[4] || isStanding[5] ||
+			isStanding[7] || isStanding[8] || isStanding[9]) {
 		fmt.Println("Yes")
 		return
 	}
 
 	// 3 と 9 が倒れている
-	if !isStanding[2] && !isStanding[8] && (isStanding[5] || isStanding[9]) && (isStanding[1] || isStanding[3] || isStanding[4] || isStanding[6] || isStanding[7]) {
+	if !isStanding[2] && !isStanding[8] &&
+		(isStanding[5] || isStanding[9]) &&
+		(isStanding[1] || isStanding[3] ||
+			isStanding[4] || isStanding[6] || isStanding[7]) {
 		fmt.Println("Yes")
 		return
 	}
 
 	// 6 が倒れている
-	if !isStanding[5] && isStanding[9] && (isStanding[1] || isStanding[2] || isStanding[3] || isStanding[4] || isStanding[6] || isStanding[7] || isStanding[8]) {
+	if !isStanding[5] && isStanding[9] &&
+		(isStanding[1] || isStanding[2] ||
+			isStanding[3] || isStanding[4] ||
+			isStanding[6] || isStanding[7] || isStanding[8]) {
 		fmt.Println("Yes")
 		return
 	}
